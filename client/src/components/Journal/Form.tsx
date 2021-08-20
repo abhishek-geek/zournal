c5d967fda07aa3ade538731f8e685b69e8cc6bc8
abhishek-geek <2019005@iiitdmj.ac.in>
2021-08-20 21:40:06 +0530

    adding new journal to the state using form and dispatcher

--- client/src/components/Journal/Form.tsx
+++ client/src/components/Journal/Form.tsx
@@ -1,13 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { addJournal } from "../../reducers/journalReducer";
 import Button from "../ui/Button";
 // import Input from "../ui/Input";
 import "./journal.css";
 
 const Form = () => {
+  const [content, setContent] = useState("");
+  const dispatch = useDispatch();
+
   const handleForm = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
     e.preventDefault();
-    console.log("Form");
+    console.log(content);
+    const d = new Date();
+    const entry = {
+      id: Number(d.getTime()),
+      date: d,
+      content,
+    };
+    console.log(entry);
+
+    dispatch(addJournal(entry));
   };
+
   return (
     <div className="bg-green-100 mx-5">
       <form className="">
@@ -17,6 +32,8 @@ const Form = () => {
           rows={4}
           cols={50}
           placeholder="Start Writing..."
+          value={content}
+          onChange={(e) => setContent(e.target.value)}
         />
         <Button
           className="journal-submit"
