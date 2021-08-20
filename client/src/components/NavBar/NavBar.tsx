dd0b249f2b6797f8d4d0b05fbfa8720f8bf9dfaf
abhishek-geek <2019005@iiitdmj.ac.in>
2021-08-20 15:46:08 +0530

    navbar for mobile

--- client/src/components/NavBar/NavBar.tsx
+++ client/src/components/NavBar/NavBar.tsx
@@ -1,31 +1,80 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import Button from "../ui/Button";
 import "./nav.css";
+import ham from "./ham.png";
+import back from "./back.png";
 
 const NavBar = () => {
+  const [user, setUser] = useState(false);
+  const [open, setOpen] = useState("");
+  const [navs, setNavs] = useState("");
+  const [close, setClose] = useState("hidden");
+
+  const showNav = () => {
+    console.log("clicked open");
+    setClose("");
+    setNavs("active");
+    setOpen("hidden");
+  };
+
+  const hideNav = () => {
+    console.log("clicked close");
+    setClose("hidden");
+    setNavs("");
+    setOpen("");
+  };
+
   return (
     <div className="bg-gray-300 navbar -mt-12">
       <div className="logo">
         <Link to="/">Zournal</Link>
       </div>
-      <div className="navs">
+      <div className="ham">
+        <img
+          onClick={showNav}
+          className={`open ${open}`}
+          src={ham}
+          alt="menu"
+        />
+        <img
+          onClick={hideNav}
+          className={`close ${close}`}
+          src={back}
+          alt="menu"
+        />
+      </div>
+      <div className={`navs ${navs}`}>
         <ul>
           <li>
             <Link to="/">
-              <Button value="Home" className="btn" />
+              <Button value="Home" className="btn w-100" />
             </Link>
           </li>
-          <li>
-            <Link to="/login">
-              <Button value="Login" />
-            </Link>
-          </li>
-          <li>
-            <Link to="/register">
-              <Button value="Register" />
-            </Link>
-          </li>
+          {user && (
+            <div className="user name">
+              <li>
+                <Link to="/logout">
+                  <Button value="Logout" className="w-100" />
+                </Link>
+              </li>
+              {/* <span>Abhishek Dubey</span> */}
+            </div>
+          )}
+          {!user && (
+            <div className="user">
+              <li>
+                <Link to="/login">
+                  <Button value="Login" className="w-100" />
+                </Link>
+              </li>
+              <li>
+                <Link to="/register">
+                  <Button value="Register" className="w-100" />
+                </Link>
+              </li>
+            </div>
+          )}
         </ul>
       </div>
     </div>
